eb6d6f1e873df0fd23b5851a26dd6a1de225f8b6
DiahPrastyaDewwi <125942668+DiahPrastyaDewwi@users.noreply.github.com>
2023-08-30 22:11:07 +0800

    tambah biodata

---
+++ biodata_diah.py
@@ -0,0 +1,20 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Diah Prastya"
+hobi = "Mendengarkan lagu"
+alamat = "Br.Kedampal"
+Kota = "Tabanan"
+Propinsi = "Bali"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 
